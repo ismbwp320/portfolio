d8f501c0bfd71785f37df2945d41096934d7cfd8
Muhammad Ismail <ismbwp320@gmail.com>
2022-02-08 23:58:33 +0500

    Initial commit

---
+++ src/Application.js
@@ -0,0 +1,11 @@
+function Application() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Learn React
+      </header>
+    </div>
+  );
+}
+
+export default Application;
